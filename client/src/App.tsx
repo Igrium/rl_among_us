163fba7ad9299453e926977046eb0ef4095f7eaf
Sam <thesam54123@gmail.com>
2020-12-30 12:50:14 -0800

    wrote connection handler in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,27 @@
 import React, { useEffect, Component } from 'react';
-import logo from './logo.svg';
-import socketIOClient from 'socket.io-client';
 import './App.css';
-import ClientTestComponent from './ClientTestComponent';
+import LoginScreen from './components/LoginScreen';
+import ConnectionHandler, {IConnectionInfo} from './logic/ConnectionHandler'
 
 
 
 function App() {
 
-  useEffect(() => {
-    const socket = socketIOClient.io('http://localhost:5000', {transports: ['websocket']});
-    socket.on('disconnectWarning', (message: any, data: any) => {
-      console.log('test');
-    })
-  })
+    function handleSubmit(connectionInfo: any) {
+        ConnectionHandler.activeConnection = new ConnectionHandler({
+            url: connectionInfo.url,
+            playerName: connectionInfo.playerName
+        }, () => {
+            alert(`Connected to ${connectionInfo.url} as ${connectionInfo.playerName}`);
+        })
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ClientTestComponent />
-    </div>
+    return (
+        <div className="App">
+            <LoginScreen onSubmit={handleSubmit} />
+        </div>
 
-  );
+    );
 }
 
 export default App;
---
+++ client/src/logic/ConnectionHandler.ts
@@ -0,0 +1,48 @@
+import socketIOClient from 'socket.io-client';
+import events from 'events';
+
+export interface IConnectionInfo {
+    url: string,
+    playerName: string
+}
+
+class ConnectionHandler {
+
+    private _connectionInfo: IConnectionInfo = { url: '', playerName: '' };
+    private _io: SocketIOClient.Socket
+
+    constructor(connectionInfo: IConnectionInfo, callback?: () => void) {
+        this._connectionInfo = connectionInfo;
+        const { url, playerName } = connectionInfo;
+        const connectionHeader = { connectionType: 'player', name: playerName }
+        this._io = socketIOClient(url,
+            { transports: ['websocket'], query: `connectionInfo=${JSON.stringify(connectionHeader)}` }
+        )
+        if (callback != undefined) {
+            this.io.on('connect', callback);
+        }
+    }
+
+    public get connectionInfo(): IConnectionInfo {
+        return this._connectionInfo;
+    }
+
+    public get io(): SocketIOClient.Socket {
+        return this._io;
+    }
+
+    // EVENTS
+    emitter = new events.EventEmitter();
+
+    /**
+     * Called when the server tells the client that it's going to be disconnected.
+     * @param listener Event listener.
+     */
+    public onDisconnectWarning(listener: (reason: string) => void): void {
+        this.emitter.on('disconnectWarning', listener)
+    }
+
+    public static activeConnection: ConnectionHandler
+}
+
+export default ConnectionHandler;
